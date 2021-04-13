879b17899a2b3c8d243b143b528bd4bed2760979
sancbein <sancbein@gmail.com>
2021-04-13 05:50:05 -0400

    java program edited

--- Simple.java
+++ Simple.java
@@ -1,5 +1,8 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+    for (int i=1;i<=10;i++)
+{
+     System.out.println("Hello Java World...! +i");
+}  
     }  
 }  
